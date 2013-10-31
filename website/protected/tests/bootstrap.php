b1d0e863ace798cf4945d1a3915fe21110626cf4
Eugene Batalov <eabatalov89@gmail.com>
2013-10-31 11:43:58 +0400

    website: added unit tests
    Forgotten to git add many implemented already tests.
    Implemented almost all the changes tracking tests for model classes.

---
+++ website/protected/tests/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @author eugene
+ */
+
+/*
+ * Setting up phpunit:
+ * sudo pear install pear.phpunit.de/PHPUnit
+ * sudo pear install phpunit/PHPUnit_SkeletonGenerator
+ */
+//all the paths are relative to website/protected/tests
+set_include_path(get_include_path() . PATH_SEPARATOR .
+        "../models/");
+
+require_once \dirname(__FILE__).'/../external/yii/framework/yii.php';
+
+function autoloader($className) {
+    //$classPath =
+        //"/data/github/educational-gaming-platform/website/protected/models/"
+        //. $className;
+    print($className . "\n");
+    include_once($className . ".php");
+}
+
+spl_autoload_register('autoloader'); 
+?>
