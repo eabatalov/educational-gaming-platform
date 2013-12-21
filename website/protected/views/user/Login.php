a589307b95e3e93dfd15f049c757af4b1eb86b2d
Eugene Batalov <eabatalov89@gmail.com>
2013-12-22 01:32:22 +0400

    website: added Login.php page which uses Learzing API

---
+++ website/protected/views/user/Login.php
@@ -0,0 +1,28 @@
+<div class="row">
+    <label for="email">Email</label>
+    <input type="text" name="email" id="input_email" value="4_Email@example.com"/>
+</div>
+<div class="row">
+    <label for="password">Password</label>
+    <input type="password" name="password" id="input_password" value="Password_4"/>
+</div>
+
+<a class="btn" onclick="doLogin();">Login</a>
+
+<script type="text/javascript">
+    function doLogin() {
+        var email = $("#input_email").val();
+        var password = $("#input_password").val();
+        LEARZ.auth.login(email, password, doLoginCallback);
+    }
+
+    function doLoginCallback(response) {
+          if (response.status === LEARZING_STATUS_SUCCESS) {
+              alert(response.data);
+          } else {
+              alert("Errors have occured: \n" +
+                response.texts.toString() + "\n" +
+                "Learzing API status: " + response.status);
+          }
+    }
+</script>
