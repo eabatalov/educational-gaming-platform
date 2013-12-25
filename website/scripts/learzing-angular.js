52f761f41243e7bbd10c1f70d5e88a124008f151
Eugene Batalov <eabatalov89@gmail.com>
2013-12-26 00:22:50 +0400

    website learzing-angular: implemented ng-focuse derective which allows to use boolean $focused variable in ng expressions

--- website/scripts/learzing-angular.js
+++ website/scripts/learzing-angular.js
@@ -25,3 +25,22 @@ LearzingLogoutModule.controller('LearzingLogoutController', ['$scope', 'LEARZ',
         };
     }
 ]);
+
+var LearzingAngularExtensionsModule = angular.module('LearzingAngularExtensionsModule', []);
+LearzingAngularExtensionsModule.directive('ngFocus', [function() {
+    var FOCUS_CLASS = "ng-focused";
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attrs, ctrl) {
+            ctrl.$focused = false;
+            element.bind('focus', function(evt) {
+                element.addClass(FOCUS_CLASS);
+                scope.$apply(function() {ctrl.$focused = true;});
+            }).bind('blur', function(evt) {
+                element.removeClass(FOCUS_CLASS);
+                scope.$apply(function() {ctrl.$focused = false;});
+            });
+        }
+    };
+ }]);
