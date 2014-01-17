bbc010f813e179254c7ed24a3c36a64f4c6dcea8
eugene <eabatalov89@gmail.com>
2014-01-17 19:30:01 +0000

    website: introduce local config
    This is needed to make different settings for each environment
    Require local config file not to forget about it when we deploy to another host.

--- website/index.php
+++ website/index.php
@@ -4,8 +4,6 @@
 $yii=dirname(__FILE__).'/protected/external/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
-// remove the following line when in production mode
-//defined('YII_DEBUG') or
-define('YII_DEBUG',true);
+require_once(dirname(__FILE__).'/protected/config/local.config.php');
 require_once($yii);
 Yii::createWebApplication($config)->run();
